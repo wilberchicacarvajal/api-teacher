3db3b2ebaef0ea2fc3ac26e804e32d06beaf03c1
wilberchicacarvajal <wilberpros@gmail.com>
2023-05-16 21:40:23 -0500

    se crea el servidor y se pone a correr la api

--- src/infra/repositories/TeacherRepository.ts
+++ src/infra/repositories/TeacherRepository.ts
@@ -3,6 +3,9 @@ import { Teacher } from './../../domain/entities/Teacher';
 import { ITeacherRepository } from './../../domain/interfaces/ITeacherRepository';
 
 export class TeacherRepository implements ITeacherRepository {
+    getRoutes(): import("express-serve-static-core").RequestHandler<{}, any, any, import("qs").ParsedQs, Record<string, any>> {
+        throw new Error('Method not implemented.');
+    }
     private _prisma: PrismaClient;
     constructor() {
         this._prisma = new PrismaClient();
